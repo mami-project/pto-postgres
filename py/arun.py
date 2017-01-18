2830cb62305f04338773fa5bfbad8eb93346665e
Brian Trammell <brian@trammell.ch>
2017-01-18 10:45:02 +0100

    begin sketching analysis runtime

---
+++ py/arun.py
@@ -0,0 +1,42 @@
+#
+# Path Transparency Observatory
+# Analysis Runtime
+#
+
+class AnalysisContext:
+
+    def create_observation_set(self, ):
+        pass
+
+class ObservationSetWriter:
+
+    def observe(self, start, end, path, condition, value):
+        pass
+
+    def complete(self):
+        """
+        Finish writing observations to this observation set
+        """
+        pass
+
+class ObservationSetReader:
+
+    def __iter__(self):
+        """
+        Iterate over observations in this observation set
+        """
+        return self
+
+    def __next__(self):
+        """
+        Get the next observation in the observation set, when used as an iterator
+        """
+        pass
+
+class RawAnalyzer:
+    def run(self, context, some_handle_to_spark):
+        pass
+
+class ObservationAnalyzer:
+    def run(self, context, reader):
+        pass
