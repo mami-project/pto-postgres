bac545caf0ab708dbd696136efee012c3b73c888
mirja <mirja.kuehlewind@tik.ee.ethz.ch>
2017-01-19 14:35:50 +0100

    creating an observation set and insert an observation

--- py/arun.py
+++ py/arun.py
@@ -3,17 +3,27 @@
 # Analysis Runtime
 #
 
+from ptoweb import get_db
+
 class AnalysisContext:
 
-    def __init__(self, ):
+    def __init__(self):
         pass
         
-    def create_observation_set(self, ):
-        pass
+    def create_observation_set(self, name):
+        set = get_db().insert('observation_set', name=name)
+        return ObservationSetWriter(set.id)
 
 class ObservationSetWriter:
+    
+    def __init__(self, set_id):
+        self.observation_set_id = set_id
 
-    def observe(self, start, end, path, condition, value):
+    def observe(self, start_time, end_time, path, condition, value):
+        if vaue is None:
+            get_db().insert('observation', full_path=path, time_from=start_time, time_to=end_time, condition=condition, observation_set=self.observation_set_id)
+        else:
+            get_db().insert('observation', full_path=path, time_from=start, time_to=end, condition=condition, val_n=value, observation_set=self.observation_set_id)
         pass
 
     def complete(self):
