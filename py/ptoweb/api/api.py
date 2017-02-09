fc7bad91ed6488dab8a80b94b722137f07e1e1eb
Roman Muentener <fmnssun@gmail.com>
2017-02-09 14:15:29 +0100

    + [API] raw/upload method

--- py/ptoweb/api/api.py
+++ py/ptoweb/api/api.py
@@ -12,10 +12,31 @@ from pg import escape_string
 import os
 import os.path
 from werkzeug.utils import secure_filename
+import hashlib
 
 def sha1_hash(s):
   return hashlib.sha1(s.encode("utf-8")).hexdigest()
 
+
+def sha1_file_hash(fobj, chunk_size = 8192):
+  """
+  Calculate sha1 hash of a file object chunk-wise.
+
+  Args:
+    fobj - File object (read(size))
+    chunk_size - Size of chunks to read (default 8192)
+
+  Returns: sha1 hash as string, hex/lowercase
+  """
+
+  md = hashlib.sha1()
+  while True:
+    chunk = fobj.read(chunk_size)
+    if len(chunk) == 0:
+      break
+    md.update(chunk)
+
+  return md.hexdigest()
 
 
 class CustomEncoder(json.JSONEncoder):
@@ -55,6 +76,71 @@ def to_int(value):
     return 0
 
 
+def insert_upload(filesystem_path, campaign, file_hash, start_time, stop_time, uploader, metadata):
+  """
+  Insert an upload
+  """
+
+  filesystem_path = escape_string(filesystem_path)
+  campaign = escape_string(campaign)
+  file_hash = escape_string(file_hash)
+  uploader = escape_string(uploader)
+  metadata = escape_string(json.dumps(metadata))
+
+  sql = """
+  INSERT INTO uploads(
+    filesystem_path,
+    campaign,
+    file_hash,
+    start_time,
+    stop_time,
+    uploader,
+    metadata,
+    time_of_upload)
+
+  VALUES(
+    '%s',
+    '%s',
+    '%s',
+    to_timestamp(%d) :: TIMESTAMP WITHOUT TIME ZONE,
+    to_timestamp(%d) :: TIMESTAMP WITHOUT TIME ZONE,
+    '%s',
+    '%s' :: JSONb,
+    to_timestamp(%d) :: TIMESTAMP WITHOUT TIME ZONE);
+  """ % (filesystem_path, campaign, file_hash, start_time, stop_time, uploader, metadata, datetime.utcnow().timestamp())
+
+  get_db().query(sql)
+
+  return True
+
+def check_permissions(key, required_permissions):
+  """
+  Check permissions based on api key
+  """
+
+  if not isinstance(key, str):
+    return False
+
+  try:
+    for e in get_db().query("SELECT * FROM api_keys WHERE key = '%s';" % (escape_string(key))).dictresult():
+      if not 'permissions' in e:
+        return False
+
+      permissions = e['permissions']
+
+      if not isinstance(permissions, str):
+        return False
+
+      for required_permission in required_permissions:
+        if not required_permission in permissions:
+          return False
+
+      return e['name']
+  except Exception as error:
+    print(error)
+    return False
+
+  return False
 
 @app.route('/')
 def api_index():
@@ -225,6 +311,13 @@ def api_qq_get():
 
 @app.route('/raw/upload', methods=['POST'])
 def api_raw_upload():
+  key = request.headers.get('X-API-KEY')
+
+  uploader = check_permissions(key, 'u')
+
+  if not uploader:
+    return json400({"error": "Insufficient permissions or invalid API key"}) 
+
   if 'data' not in request.files:
     return json400({"error":"File is missing"})
 
@@ -245,7 +338,19 @@ def api_raw_upload():
   if not isinstance(metadata['filename'], str) or not isinstance(metadata['campaign'], str):
     return json400({"error" : "Wrong type for filename and/or campaign"})
 
+  if (not ('start_time' in metadata)) or (not ('stop_time' in metadata)):
+    return json400({"error" : "Missing start_time and/or stop_time!"})
+
+  if not isinstance(metadata['start_time'], int) or not isinstance(metadata['stop_time'], int):
+    return json400({"error" : "Wrong type for start_time and/or stop_time!"})
+
   data = request.files['data']
+
+  file_hash = sha1_file_hash(data.stream)
+  start_time = metadata['start_time']
+  stop_time = metadata['stop_time']
+  campaign = metadata['campaign']
+
   secure_campaign_ = secure_filename(metadata['campaign'])
   secure_filename_ = secure_filename(metadata['filename'])
   path = os.path.join(secure_campaign_, secure_filename_)
@@ -262,4 +367,10 @@ def api_raw_upload():
 
   data.save(os.path.join(path_prefix, path))
 
-  return json200({"msg":"OK"})
+  try:
+    insert_upload(save_path, campaign, file_hash, start_time, stop_time, uploader, metadata)
+  except Exception as error:
+    print(error)
+    return json500({"error" : "Internal error!"})
+
+  return json200({"file_hash":file_hash})
